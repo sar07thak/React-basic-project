648999c6f7a1155b5f63411da7435556a7f3fc54
sarthak <sarthak75220@gmail.com>
2024-12-19 09:02:46 +0530

    setup

---
+++ upto-useState/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+     <div className='bg-purple-600 h-screen'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non exercitationem, obcaecati sequi at amet suscipit delectus totam qui. Doloremque deserunt aliquam labore quaerat cum consectetur. Ducimus debitis explicabo minima quaerat.
+     </div>
+  )
+}
+
+export default App
